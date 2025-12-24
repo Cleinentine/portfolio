aa4efb353eeb396a2aeac442c9180f96a7fc31d1
Clein <cleinentine@gmail.com>
2025-12-24 09:43:18 +0800

    1.4 - Fixed Padding

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <section class="font-montserrat md:text-left mt-25 text-center">
+  <section class="font-montserrat md:text-left mt-25 p-5 text-center">
     <h2 class="font-bold font-nunito italic text-2xl">Hello, I'm</h2>
     <h1 class="font-black mt-25 text-7xl uppercase">Clein Wepee</h1>
     <h3 class="font-light mt-2"><span class="text-violet-400">Laravel</span> Developer from Philippines.</h3>
